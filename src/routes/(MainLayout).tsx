731932b875d99337c22cecc204c1b196205a38bf
rayhan <rayhan.rhssoe2@gmail.com>
2023-11-23 21:35:40 +0700

    add cart context provider.

--- src/routes/(MainLayout).tsx
+++ src/routes/(MainLayout).tsx
@@ -1,11 +1,5 @@
-import { For, Show, createEffect } from "solid-js";
-import {
-	A,
-	Outlet,
-	refetchRouteData,
-	useIsRouting,
-	useRouteData,
-} from "solid-start";
+import { For, Show } from "solid-js";
+import { A, Outlet, useRouteData } from "solid-start";
 import { createServerData$, useRequest } from "solid-start/server";
 import Footer from "~/components/Footer";
 import MobileMenu from "~/components/layout/navbar/mobile-menu";
@@ -16,6 +10,7 @@ import Search from "~/components/layout/navbar/search";
 import { Suspense } from "~/components/solid/Suspense";
 import { createCart, getCart } from "~/lib/shopify";
 import { getCookieObject } from "~/utils/cookie";
+import { CartProvider } from "~/context/CartActionContext";
 
 export function routeData() {
 	const cart = createServerData$(
@@ -64,77 +59,66 @@ export function routeData() {
 		}
 	);
 
-	// const dummy = createRouteData(getDummy);
-
 	return { cart };
 }
 
 export default function MainLayout() {
 	const { cart } = useRouteData<typeof routeData>();
-	const isRouting = useIsRouting();
-	const navMenu = [
-		{ title: "All", path: "/search" },
-		// { title: "Shirts", path: "/search/shirts" },
-		// { title: "Merch", path: "/search/merch" },
-	];
-
-	createEffect(() => {
-		if (isRouting()) {
-			refetchRouteData("cart");
-		}
-	});
+	const navMenu = [{ title: "All", path: "/search" }];
 
 	return (
 		<>
-			<nav class="relative flex items-center justify-between p-4 lg:px-6">
-				<div class="block flex-none md:hidden">
-					<MobileMenu menu={navMenu} />
-				</div>
-				<div class="flex w-full items-center">
-					<div class="flex w-full md:w-1/3">
-						<A
-							href="/"
-							class="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6"
-						>
-							<LogoSquare />
-							<div class="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
-								{import.meta.env.VITE_SITE_NAME}
-							</div>
-						</A>
+			<CartProvider>
+				<nav class="relative flex items-center justify-between p-4 lg:px-6">
+					<div class="block flex-none md:hidden">
+						<MobileMenu menu={navMenu} />
+					</div>
+					<div class="flex w-full items-center">
+						<div class="flex w-full md:w-1/3">
+							<A
+								href="/"
+								class="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6"
+							>
+								<LogoSquare />
+								<div class="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
+									{import.meta.env.VITE_SITE_NAME}
+								</div>
+							</A>
 
-						<Show when={navMenu.length}>
-							<ul class="hidden gap-6 text-sm md:flex md:items-center">
-								<For each={navMenu}>
-									{(item) => (
-										<li>
-											<A
-												href={item.path}
-												class="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
-											>
-												{item.title}
-											</A>
-										</li>
-									)}
-								</For>
-							</ul>
-						</Show>
+							<Show when={navMenu.length}>
+								<ul class="hidden gap-6 text-sm md:flex md:items-center">
+									<For each={navMenu}>
+										{(item) => (
+											<li>
+												<A
+													href={item.path}
+													class="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
+												>
+													{item.title}
+												</A>
+											</li>
+										)}
+									</For>
+								</ul>
+							</Show>
+						</div>
+						<div class="hidden justify-center md:flex md:w-1/3">
+							<Search />
+						</div>
+						<div class="flex justify-end md:w-1/3">
+							<Suspense fallback={<OpenCart />}>
+								<Show when={cart()}>{(cart) => <CartModal cart={cart()} />}</Show>
+							</Suspense>
+						</div>
 					</div>
-					<div class="hidden justify-center md:flex md:w-1/3">
-						<Search />
-					</div>
-					<div class="flex justify-end md:w-1/3">
-						<Suspense fallback={<OpenCart />}>
-							<Show when={cart()}>{(cart) => <CartModal cart={cart()} />}</Show>
-						</Suspense>
-					</div>
-				</div>
-			</nav>
-			{/* <SuspenseList revealOrder="together"> */}
-			<Suspense fallback={<h1>Loading...</h1>}>
-				<Outlet />
-			</Suspense>
-			{/* </SuspenseList> */}
-			<Footer />
+				</nav>
+				{/* <SuspenseList revealOrder="together"> */}
+				<Suspense fallback={<h1>Loading...</h1>}>
+					<Outlet />
+				</Suspense>
+				{/* </SuspenseList> */}
+				<Footer />
+			</CartProvider>
 		</>
 	);
 }
