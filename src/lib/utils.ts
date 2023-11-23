ce7951ea7275eba90ed909b16e7bd16e1ba7e7be
rayhan <rayhan.rhssoe2@gmail.com>
2023-11-23 21:32:51 +0700

    Dynamic API URL.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,3 +7,7 @@ export const createUrl = (pathname: string, params: URLSearchParams) => {
 
 export const ensureStartsWith = (stringToCheck: string, startsWith: string) =>
   stringToCheck.startsWith(startsWith) ? stringToCheck : `${startsWith}${stringToCheck}`;
+
+  const adapter = import.meta.env.VITE_ADAPTER as string
+
+  export const API_URL = import.meta.env.DEV ? 'http://localhost:3000' : `https://${import.meta.env.VITE_SITE_DOMAIN}.${adapter.toLowerCase()}.app/`
